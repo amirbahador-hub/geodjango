b61fd4881b531d2da09da227e21bc431c1fca121
Amirbahador Pv <amirbahador.pv@gmail.com>
2021-01-09 12:07:27 -0500

    add a log if a product save

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+import logging
+logger = logging.getLogger('interface')
+
 
 class Product(models.Model):
     title = models.CharField(max_length=1024)
@@ -7,4 +10,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def __save__(self, *args, **kwargs):
+        logger.debug({
+            'section': 'views',
+            'code': 'unexpected_error',
+            'object': self.title,
+            'price': self.price
+        })
+        super().save(*args, **kwargs)
 
