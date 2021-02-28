9b4200bc6e82a6d0fc586024d190e78e55c42d67
Amirbahador Pv <amirbahador.pv@gmail.com>
2021-02-28 10:15:09 -0500

    add panle admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,10 @@
 from django.contrib.gis import admin
-from .models import WorldBorder
+from .models import WorldBorder, Source, Destination, Product, Zipcode, Elevation
+
 
 admin.site.register(WorldBorder, admin.GeoModelAdmin)
+admin.site.register(Source, admin.GeoModelAdmin)
+admin.site.register(Destination, admin.GeoModelAdmin)
+admin.site.register(Product, admin.GeoModelAdmin)
+admin.site.register(Zipcode, admin.GeoModelAdmin)
+admin.site.register(Elevation, admin.GeoModelAdmin)
