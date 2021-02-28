a461a34a0a7fdf784139773dd56f689ee707f002
Amirbahador Pv <amirbahador.pv@gmail.com>
2021-02-28 07:17:57 -0500

    connect postgres dbs

--- GeoDjango/settings.py
+++ GeoDjango/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'GeoDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+         'NAME': 'geotest',
+         'USER': 'amirbahador',
+         'PASSWORD': '13771998g',
+         'HOST': 'localhost',
+         'PORT': '5432',
+    },
 }
 
 
